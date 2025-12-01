dfd3746998c38ce6564a0780a6886bde7674563c
Thiago Thomas <thiago_rocha17@hotmail.com>
2025-12-01 22:58:29 +0000

    feat: enhance layout with responsive design (tablet and desktop)

--- src/App.tsx
+++ src/App.tsx
@@ -90,111 +90,124 @@ function App() {
 
   return (
     <Layout>
-      <h2>Found {filteredCountries.length} countries</h2>
-      <div className="app__input-container">
-        <input
-          type="text"
-          className="app__search-input"
-          placeholder="Search by Name, Capital"
-          onChange={e => setSearch(e.target.value)}
-        />
+      <div className="header-container">
+        <h2>Found {filteredCountries.length} countries</h2>
+        <div className="app__input-container">
+          <input
+            type="text"
+            className="app__search-input"
+            placeholder="Search by Name, Capital"
+            onChange={e => setSearch(e.target.value)}
+          />
+        </div>
       </div>
 
-      <div className="app__input-container">
-        <label htmlFor="population">Sort by</label>
-        <select
-          name="population"
-          id="population"
-          className="app__select-input"
-          onChange={e => setSort(e.target.value)}
-        >
-          <option value="name">Name</option>
-          <option value="capital">Capital</option>
-          <option value="area">Area</option>
-        </select>
-      </div>
-
-      <div className="app__input-container">
-        <label>Region</label>
-        <div className="app__region-container">
-          {regions.map(region => (
-            <button
-              key={region}
-              type="button"
-              className={`app__region-container--buttons ${selectedRegions.includes(region) ? 'active' : ''}`}
-              onClick={() => handleRegionChange(region)}
+      <div className="grid-main-container">
+        <div className="grid-left">
+          <div className="app__input-container">
+            <label htmlFor="population">Sort by</label>
+            <select
+              name="population"
+              id="population"
+              className="app__select-input"
+              onChange={e => setSort(e.target.value)}
             >
-              {region}
-            </button>
-          ))}
-        </div>
-      </div>
-
-      <div className="app__input-container">
-        <label>Status</label>
-        <div className="app__status-container">
-          <div className="app__status-container--checkbox">
-            <input
-              type="checkbox"
-              id="UnitedNations"
-              title="Member of the United Nations"
-              checked={status.unMember}
-              onChange={handleStatusChange}
-            />
-            <label htmlFor="UnitedNations">Member of the United Nations</label>
+              <option value="name">Name</option>
+              <option value="capital">Capital</option>
+              <option value="area">Area</option>
+            </select>
           </div>
-          <div className="app__status-container--checkbox">
-            <input
-              type="checkbox"
-              id="Independent"
-              title="Independent"
-              checked={status.independent}
-              onChange={handleStatusChange}
-            />
-            <label htmlFor="Independent">Independent</label>
-          </div>
-        </div>
-      </div>
 
-      <table className="app__country-grid-container">
-        <thead>
-          <tr>
-            <th>
-              <span>Flag</span>
-            </th>
-            <th>
-              <span>Name</span>
-            </th>
-            <th>
-              <span>Capital</span>
-            </th>
-            <th>
-              <span>Area (km²)</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredCountries.map(country => (
-            <tr>
-              <td>
-                <img
-                  src={`https://flagcdn.com/w80/${country.cca2.toLowerCase()}.png`}
-                  alt={`Flag of ${country.name.common}`}
+          <div className="app__input-container">
+            <label>Region</label>
+            <div className="app__region-container">
+              {regions.map(region => (
+                <button
+                  key={region}
+                  type="button"
+                  className={`app__region-container--buttons ${selectedRegions.includes(region) ? 'active' : ''}`}
+                  onClick={() => handleRegionChange(region)}
+                >
+                  {region}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          <div className="app__input-container">
+            <label>Status</label>
+            <div className="app__status-container">
+              <div className="app__status-container--checkbox">
+                <input
+                  type="checkbox"
+                  id="UnitedNations"
+                  title="Member of the United Nations"
+                  checked={status.unMember}
+                  onChange={handleStatusChange}
                 />
-              </td>
-              <td>
-                <span>{country.name.common}</span>
-              </td>
-              <td>
-                <span>{country.capital?.[0] || '(no capital)'}</span>
-              </td>
-              <td>
-                <span>{country.area}</span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                <label htmlFor="UnitedNations">Member of the United Nations</label>
+              </div>
+              <div className="app__status-container--checkbox">
+                <input
+                  type="checkbox"
+                  id="Independent"
+                  title="Independent"
+                  checked={status.independent}
+                  onChange={handleStatusChange}
+                />
+                <label htmlFor="Independent">Independent</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid-right">
+          <table className="app__country-grid-container">
+            <thead>
+              <tr>
+                <th>
+                  <span>Flag</span>
+                </th>
+                <th>
+                  <span>Name</span>
+                </th>
+                <th>
+                  <span>Capital</span>
+                </th>
+                <th>
+                  <span>Area (km²)</span>
+                </th>
+                <th className="region-col">
+                  <span>Region</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredCountries.map(country => (
+                <tr>
+                  <td>
+                    <img
+                      src={`https://flagcdn.com/w80/${country.cca2.toLowerCase()}.png`}
+                      alt={`Flag of ${country.name.common}`}
+                    />
+                  </td>
+                  <td>
+                    <span>{country.name.common}</span>
+                  </td>
+                  <td>
+                    <span>{country.capital?.[0] || '(no capital)'}</span>
+                  </td>
+                  <td>
+                    <span>{country.area}</span>
+                  </td>
+                  <td className="region-col">
+                    <span>{country.region}</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </Layout>
   );
 }
