cbfd707e2a7a0e29158f22deb8ec095a9491659f
Thiago Thomas <thiago_rocha17@hotmail.com>
2026-01-05 19:40:43 +0000

    fix: update image imports in MainLayout for correct paths and responsive design

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from 'react';
 import './style.css';
+import Logo from '../../assets/Logo.svg';
+import HeroImgSm from '../../assets/hero-image-sm.jpg';
+import HeroImg from '../../assets/hero-image.jpg';
+
 
 type LayoutProps = {
   children: ReactNode;
@@ -10,9 +14,9 @@ export function MainLayout({ children }: LayoutProps) {
     <div className="layout">
       <header className="layout__header">
         <div className="layout__logo_container">
-          <img src="../../src/assets/Logo.svg" alt="Logo" className="img-logo" />
+          <img src={Logo} alt="Logo" className="img-logo" />
         </div>
-        <img src="../../src/assets/hero-image-sm.jpg" alt="Hero Image" className="img-hero" />
+        <img src={HeroImgSm} srcSet={`${HeroImgSm} 600w, ${HeroImg} 1024w`} alt="Hero Image" className="img-hero" />
       </header>
       <main className="layout__main">{children}</main>
     </div>
