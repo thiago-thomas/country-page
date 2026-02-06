e6483b43524c136fe9deb6b34eb130b4ceaaabb1
Thiago Thomas <thiago_rocha17@hotmail.com>
2026-02-06 19:07:28 +0000

    feat: add InfoPill component and integrate it into CountryPage for displaying the Country Header Information

--- src/pages/CountryPage/index.tsx
+++ src/pages/CountryPage/index.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { fetchCountryByName } from '../../services/api';
 import type { Country } from '../../types/api';
+import {  InfoPill } from '../../components/InfoPill'
 import './style.css';
 
 export function CountryPage() {
   const { name } = useParams<{ name: string }>();
   const navigate = useNavigate();
-  const [country, setCountry] = useState<Country | undefined>();
+  const [country, setCountry] = useState<Country | null>(null);
 
   useEffect(() => {
     async function getCountryByName(countryName: string) {
@@ -15,8 +16,9 @@ export function CountryPage() {
         const countryData = await fetchCountryByName(countryName);
         if (!countryData) {
           navigate('/');
+          return;
         } else {
-          setCountry(countryData!);
+          setCountry(countryData);
         }
       } catch (err) {
         console.error(err);
@@ -27,16 +29,25 @@ export function CountryPage() {
     }
   }, []);
 
+  if (!country) {
+    navigate('/');
+    return null;
+  }
+
   return (
     <>
       <header className="country-header">
         <div className="country-header-main-info">
           <img
-            src={`https://flagcdn.com/w320/${country?.cca2.toLowerCase()}.png`}
-            alt={`Flag of ${country?.name.common}`}
+            src={`https://flagcdn.com/w320/${country.cca2.toLowerCase()}.png`}
+            alt={`Flag of ${country.name.common}`}
           />
-          <h1>{country?.name.common}</h1>
-          <p>{country?.name.official}</p>
+          <h1>{country.name.common}</h1>
+          <p>{country.name.official}</p>
+        </div>
+        <div className="country-header-data">
+          <InfoPill keyInfo='Population' valueInfo='N/A' />
+          <InfoPill keyInfo='Area(km²)' valueInfo={country.area.toString()} />
         </div>
       </header>
     </>
